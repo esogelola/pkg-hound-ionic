ee01f17e5190c7fda5cc8f1781654092b7d825b8
esogelola <esogelola@gmail.com>
2020-12-23 02:43:49 -0500

    Firebase Setup

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBizwaH3mgl6E8ZeG8nHqJmjWmPu63YSLo",
+  authDomain: "pkghound.firebaseapp.com",
+  projectId: "pkghound",
+  storageBucket: "pkghound.appspot.com",
+  messagingSenderId: "666777011571",
+  appId: "1:666777011571:web:b91035a4e9ee313bf53106",
+  measurementId: "G-VJY4DL862G",
+};
+
+export default firebaseConfig;
