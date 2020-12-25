f7e327902734bce64fa864345e112882d09c7a2f
esogelola <esogelola@gmail.com>
2020-12-25 05:19:01 -0500

    Submit Products with Firebase Firestore

--- src/pages/Submit.js
+++ src/pages/Submit.js
@@ -1,13 +1,123 @@
 import React from "react";
-import { IonContent, IonPage } from "@ionic/react";
+import useFormValidation from "../hooks/useFormValidation";
+import validateCreateProduct from "../components/Product/validateCreateProduct";
+import firebase from "../firebase";
+import UserContext from "../contexts/UserContext";
+
+import {
+  IonContent,
+  IonPage,
+  IonItem,
+  IonInput,
+  IonLabel,
+  IonRow,
+  IonButton,
+  IonCol,
+} from "@ionic/react";
 import SmallHeader from "../components/Header/SmallHeader";
 import LargeHeader from "../components/Header/LargeHeader";
-const Submit = () => {
+
+import { toast } from "../utils/toast";
+
+const INITIAL_STATE = {
+  title: "",
+  description: "",
+  url: "",
+};
+const Submit = ({ history }) => {
+  const { user } = React.useContext(UserContext);
+  const [submitting, setSubmitting] = React.useState(false);
+  const { handleSubmit, handleChange, values } = useFormValidation(
+    INITIAL_STATE,
+    validateCreateProduct,
+    handleCreate
+  );
+
+  async function handleCreate() {
+    try {
+      if (!user) {
+        history.push("/login");
+        return;
+      }
+
+      setSubmitting(true);
+      const { url, description, title } = values;
+
+      const id = firebase.db.collection("products").doc().id;
+
+      const newProduct = {
+        title,
+        url,
+        description,
+        postedBy: {
+          id: user.uid,
+          name: user.displayName,
+        },
+        voteCount: 1,
+        comments: [],
+        votes: [
+          {
+            votedBy: { id: user.uid, name: user.displayName },
+          },
+        ],
+        created: Date.now(),
+      };
+      await firebase.db.collection("products").doc(id).set(newProduct);
+      history.push("");
+    } catch (e) {
+      console.error(e);
+      toast(e.message);
+      setSubmitting(false);
+    }
+  }
   return (
     <IonPage>
       <SmallHeader title="Submit" />
       <IonContent fullscreen>
         <LargeHeader title="Submit" />
+        <IonItem lines="full">
+          <IonLabel position="floating">Title</IonLabel>
+          <IonInput
+            name="title"
+            values={values.title}
+            type="text"
+            onIonChange={handleChange}
+            required
+          ></IonInput>
+        </IonItem>
+        <IonItem lines="full">
+          <IonLabel position="floating">Description</IonLabel>
+          <IonInput
+            name="description"
+            values={values.description}
+            type="text"
+            onIonChange={handleChange}
+            required
+          ></IonInput>
+        </IonItem>
+        <IonItem lines="full">
+          <IonLabel position="floating">URL</IonLabel>
+          <IonInput
+            name="url"
+            values={values.url}
+            type="url"
+            onIonChange={handleChange}
+            required
+          ></IonInput>
+        </IonItem>
+        <IonRow>
+          <IonCol>
+            <IonButton
+              type="submit"
+              color="primary"
+              expand="block"
+              disabled={submitting}
+              onClick={handleSubmit}
+            >
+              Submit
+            </IonButton>
+          </IonCol>
+        </IonRow>
       </IonContent>
     </IonPage>
   );
