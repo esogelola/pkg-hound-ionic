1e372e4cc21b6c535c763d751953cd163fdc76b2
esogelola <esogelola@gmail.com>
2020-12-25 20:17:25 -0500

    Product Lists

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { IonContent, IonPage } from "@ionic/react";
+import ProductList from "../components/Product/ProductList";
 import SmallHeader from "../components/Header/SmallHeader";
 import LargeHeader from "../components/Header/LargeHeader";
-const Home = () => {
+
+const Home = (props) => {
   return (
     <IonPage>
-      <SmallHeader title="PKGHound" />
-      <IonContent fullscreen>
-        <LargeHeader title="PKGHound" />
+      <SmallHeader title="PKG Hound" />
+      <IonContent color="medium" fullscreen>
+        <LargeHeader title="PKG Hound" />
+        <br></br>
+        <ProductList location={props.location} />
       </IonContent>
     </IonPage>
   );
